e67f312582383f5eb1a74c8a4c9d5bc7b3d11d38
Daniel O'Keefe <33761482+DanOKeefe@users.noreply.github.com>
2020-06-11 09:06:36 -0400

    fixed typo

--- model_def.py
+++ model_def.py
@@ -6,7 +6,7 @@ def get_model():
     inputs = tf.keras.Input(shape=(200, 200, 1))
     conv_1 = tf.keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu')(inputs)
     conv_2 = tf.keras.layers.Conv2D(32, (3, 3), activation='relu')(conv_1)
-    maxpool_1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))(conv_2)
+    maxpool_1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv_2)
     drop_1 = tf.keras.layers.Dropout(0.25))(maxpool_1)
     
     conv_3 = tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu)(drop_1)
